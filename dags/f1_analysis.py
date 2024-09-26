c51e12920a22b94ce78d040301514c5f157cae6b
mobatusi <mobatusi@gmail.com>
2024-09-26 12:15:26 -0400

    update dag code to retrieve the project_id and dataset_id from environment variables, remove unnessary variables, and access SQL files from GCS

--- dags/f1_analysis.py
+++ dags/f1_analysis.py
@@ -1,72 +1,72 @@
 import os
-import pandas as pd
 import pendulum
-from airflow.decorators import dag
+from airflow import DAG
+from airflow.decorators import dag, task
+from airflow.models import Variable
 from airflow.providers.google.cloud.transfers.gcs_to_bigquery import (
-    GCSToBigQueryOperator,BigQueryInsertJobOperator
+    GCSToBigQueryOperator,
 )
-from datetime import timedelta
-from google.cloud import storage
-import json
+from airflow.providers.google.cloud.operators.bigquery import BigQueryInsertJobOperator
+from airflow.models.baseoperator import chain
 
-bucket_name = os.environ['GCS_BUCKET']
-file_path = 'config.json'
-
-# Create a client instance
-client = storage.Client()
-
-# Get the bucket
-bucket = client.get_bucket(bucket_name)
-
-# Get the blob
-blob = bucket.blob(file_path)
-
-# Download the blob as a string
-config_str = blob.download_as_string().decode('utf-8')
-
-# Load the JSON
-config = json.loads(config_str)
-
-# Access the parameters from the config
-mybucket = config['mybucket']
-dataset_id = config['dataset_id']
-service_account_path = config['service_account_path']
-
-PROJECT_ID = config['project_id']
-SQL_FOLDER = "/sql"
-DATA_FOLDER = "/data"
-
-default_args = {
-    'owner': 'airflow',
-    'depends_on_past': False,
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'retries': 1,
-    'retry_delay': pendulum.duration(minutes=5),
-}
+# Replace module-level code with Airflow Variables
+mybucket = os.environ['GCS_BUCKET']
+DATASET_ID = os.environ['dataset_id']
+PROJECT_ID = os.environ['GCP_PROJECT']
 
 @dag(
-    schedule="* * * * 1-5",
+    schedule="0 0 * * 1-5",
     start_date=pendulum.datetime(2024, 1, 1, tz="UTC"),
     catchup=False,
     tags=["educative"],
-    template_searchpath=[SQL_FOLDER],
 )
 def f1_analysis_pipeline():
-    ingestion = []
-
     # Create a task for each CSV file
     csv_files = ['drivers.csv', 'constructors.csv', 'races.csv', 'circuits.csv', 'results.csv']
 
+    # Data ingestion tasks
+    ingestion_tasks = []
     for file in csv_files:
         ingest_task = GCSToBigQueryOperator(
             task_id=f'ingest_{file}',
             bucket=mybucket,
             source_objects=[f'data/{file}'],
-            destination_project_dataset_table=f'{PROJECT_ID}.{dataset_id}.{file.split(".")[0]}',
+            destination_project_dataset_table=f'{PROJECT_ID}.{DATASET_ID}.{file.split(".")[0]}',
             source_format='CSV',
+            skip_leading_rows=1,  # Skip header row if CSV includes headers
+            autodetect=True,      # Let BigQuery autodetect the schema
             create_disposition='CREATE_IF_NEEDED',
             write_disposition='WRITE_TRUNCATE',
         )
         # Add the task to the DAG
-        ingestion.append(ingest_task)           
+        ingestion_tasks.append(ingest_task)              
+    
+    # Read SQL content from GCS
+    @task
+    def read_sql_from_gcs():
+        from google.cloud import storage
+        client = storage.Client()
+        bucket = client.bucket(mybucket)
+        blob = bucket.blob('sql/f1_analysis.sql')
+        sql_content = blob.download_as_text()
+        return sql_content
+
+    sql_content = read_sql_from_gcs()
+
+    get_final_f1_analysis = BigQueryInsertJobOperator(
+        task_id="get_final_f1_analysis",
+        project_id=PROJECT_ID,
+        configuration={
+            "query": {
+                "query": "{{ sql_content }}",
+                "useLegacySql": False,
+            }
+        },
+        params={
+            'project_id': PROJECT_ID,
+            'dataset_id': DATASET_ID,
+        },
+    )
+    ingestion_tasks >> sql_content >> get_final_f1_analysis
+
+f1_analysis_pipeline()
